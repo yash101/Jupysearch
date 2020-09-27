6c0d0c6c48d87d52d0825f0887d94704e2a17898
yash101 <lodhad@gmail.com>
2020-09-26 23:09:50 -0700

    working on connecting electron and react together

---
+++ electron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { start } = require('repl');
+const url = require('url');
+
+let mainWindow;
+
+let createWindow = function() {
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadURL(startUrl);
+    mainWindow.on('closed', function() { 
+        mainWindow = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+        app.quit();
+});
+
+app.on('activate', () => {
+    if (mainWindow === null)
+        createWindow();
+});
